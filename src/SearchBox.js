7dd9df64df1e1993bae8e779aca18dfb6e52d058
RipRed7 <anthonyharris2002@outlook.com>
2023-08-16 18:19:00 -0500

    yo yo yo?

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+	return (
+		<div className='pa3 tc'>
+			<input className=" pa2 b--green bg-light-green" type='search' placeholder='search robots' />
+		</div>
+	);
+}
+
+export default SearchBox;
