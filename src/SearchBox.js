2a3851bd630946ede8923b52080cc09a6f5015ae
RipRed7 <anthonyharris2002@outlook.com>
2023-08-16 18:35:42 -0500

    testing yo

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SearchBox = () => {
 	return (
-		<div className='pa3 tc'>
-			<input className=" pa2 b--green bg-light-green" type='search' placeholder='search robots' />
+		<div className='pa3'>
+			<input className=" pa2 b--green bg-light-blue" type='search' placeholder='search robots' />
 		</div>
 	);
 }
